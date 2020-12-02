1f9f3fe6e866063b4976b667e14409630d743741
manhiem <59243227+manhiem@users.noreply.github.com>
2020-12-02 08:40:30 +0530

    Complete Game

---
+++ Assets/Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Manager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject player1_Paddle;
+    public GameObject player1_Goal;
+
+    [Header("Player 2")]
+    public GameObject player2_Paddle;
+    public GameObject player2_Goal;
+
+    [Header("Score UI")]
+    public GameObject Player1_Text;
+    public GameObject Player2_Text;
+
+    private int Player1_Score;
+    private int Player2_Score;
+
+    public void Player1_Scored()
+    {
+        Player1_Score++;
+        Player1_Text.GetComponent<TextMeshProUGUI>().text = Player1_Score.ToString();
+        ResetPosition();
+    }
+
+    public void Player2_Scored()
+    {
+        Player2_Score++;
+        Player2_Text.GetComponent<TextMeshProUGUI>().text = Player2_Score.ToString();
+        ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        player1_Paddle.GetComponent<Paddle>().Reset();
+        player2_Paddle.GetComponent<Paddle>().Reset();
+    }
+
+
+
+}
---
+++ Assets/Player_Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Goal : MonoBehaviour
+{
+    public bool isPlayer1Goal;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+        if(!isPlayer1Goal)
+        {
+            GameObject.Find("GameManager").GetComponent<Manager>().Player1_Scored();
+
+        }
+        else
+        {
+            GameObject.Find("GameManager").GetComponent<Manager>().Player2_Scored();
+        }
+        }
+    }
+}
